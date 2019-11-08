c5a4ea11d7853678da666a0ad13aec1d3ba906ae
anandamiarta <miarta56@gmail.com>
2019-11-08 19:04:13 +0700

    Student

---
+++ application/views/pages/Student/StudentProfile.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Student - Profile</title>
+    <link rel="stylesheet" href="Style/style.css">
+    <style>
+a:link, a:visited {
+  color: black;
+  padding: 10px 25px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  border-style: solid;
+  margin: 5px;
+  height: 20px;
+  width: 150px;
+}
+a:hover, a:active {
+  background-color: red;
+}
+</style>
+</head>
+<body>
+
+<table>
+    <div class=" ">
+      <tr>
+        <th style="text-align : center; font-size : 200%" width="5000px">I-School</th>
+        <th><button type="button" style="background:red; font-size : 20px; width:100px; height:50px">Log Out</button></th>
+      </tr>
+    </div>
+  </table>
+
+  <table>
+             <tr>
+                 <th rowspan="2" width="250px" >
+                   <form>
+                      <div style="margin-left:1px; font:100; margin-top:-170px"></div>
+                      <img src="../img/student.png" alt="image" title="Legend" width="200" height="200"/>
+                     <ul>
+                          <div style="text-align:center; margin-left:-30px">Name of Student</div>
+                          <div style="text-align:center; margin-left:-30px">NISN</div>
+                     </ul>
+                     <a href="" target="_blank" >Grade</a>
+                     <a href="" target="_blank" >Attendance</a>
+                     <a href="" target="_blank" >Schedule</a>
+                     <a href="" target="_blank" >Materials</a>
+                     <a href="" target="_blank" >Assignment</a>
+                     <a href="" target="_blank" style=" background:blue">Profile</a>
+
+                 <th colspan="2" width="10000px" height="650px" >
+                   <h2> NIS           : Student NIS </h2>
+                   <h2> Full Name     : Student Name</h2>
+                   <h2> Class         : Student Class</h2>
+                   <h2> Birthday      : Student Birthday</h2>
+                   <h2> Status        : Student Status</h2>
+              <!-- <table border="1" >
+                <tr>
+                  <th width="2000px">Days</th>
+                  <th width="2000px">Time</th>
+                  <th width="2000px">Room</th>
+                  <th width="2000px">Code Subject</th>
+                  <th width="2000px">Subject</th>
+                  <th width="2000px">Class</th>
+                  <th width="2000px">Status</th>
+                </tr>
+              </table>
+              <table >
+                <tr>
+                  <th width="2000px" height="650px"></th>
+                  <th width="2000px" height="650px"></th>
+                  <th width="2000px" height="650px"></th>
+                  <th width="2000px" height="650px"></th>
+                  <th width="2000px" height="650px"></th>
+                  <th width="2000px" height="650px"></th>
+                  <th width="2000px" height="650px"></th>
+                </tr>
+              </table> -->
+              </th>
+         </table>
+
+
+</body>
+</html>
